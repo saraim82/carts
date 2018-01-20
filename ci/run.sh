8c8d305b4dfadcfebdadd8b2beab4b206565c268
Sarai Morales <Sarai_Morales@ultimatesoftware.com>
2018-01-20 08:36:43 -0500

    Adding pipeline

---
+++ ci/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -eu
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+export fly_target=${fly_target:-carts}
+echo "Concourse API target ${fly_target}"
+echo "SVC $(basename $DIR)"
+
+pushd $DIR
+  fly -t ${fly_target} set-pipeline -p carts -c pipeline.yml -n 
+  fly -t ${fly_target} unpause-pipeline -p carts
+  fly -t ${fly_target} trigger-job -w -j carts/job-build
+popd
