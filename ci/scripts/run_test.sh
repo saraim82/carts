bce70ca3a4d66dad12225c0769610ce82d579094
Sarai Morales <Sarai_Morales@ultimatesoftware.com>
2018-01-22 14:17:01 -0500

    Modifying run_test.sh

--- ci/scripts/run_test.sh
+++ ci/scripts/run_test.sh
@@ -9,8 +9,8 @@ SSH_OPTS=-oStrictHostKeyChecking=no
 
 echo "Testing $1"
 CODE_DIR=$(cd $SCRIPT_DIR/../..; pwd)
-cp $PWD/test/$1 $PWD/ci/scripts/
-cp -R $PWD/test/util $PWD/ci/scripts/
+cp $CODE_DIR/test/$1 $CODE_DIR/ci/scripts/
+cp -R $CODE_DIR/test/util $CODE_DIR/ci/scripts/
 echo "$@"
-export PYTHONPATH=\$PYTHONPATH:\$PWD/scripts
+export PYTHONPATH=\$PYTHONPATH:\$CODE_DIR/scripts
 python ci/scripts/$@
