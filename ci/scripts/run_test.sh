ce98478be0e9014a9ba4c26730efefa6671f88c7
Sarai Morales <Sarai_Morales@ultimatesoftware.com>
2018-01-22 14:29:39 -0500

    Modifying run_test.sh

--- ci/scripts/run_test.sh
+++ ci/scripts/run_test.sh
@@ -4,8 +4,6 @@ set -e -u -x
 
 SCRIPT_DIR=`dirname "$0"`
 SCRIPT_NAME=`basename "$0"`
-SSH_OPTS=-oStrictHostKeyChecking=no
-
 
 echo "Testing $1"
 CODE_DIR=$(cd $SCRIPT_DIR/../..; pwd)
@@ -13,4 +11,4 @@ cp $CODE_DIR/test/$1 $CODE_DIR/ci/scripts/
 cp -R $CODE_DIR/test/util $CODE_DIR/ci/scripts/
 echo "$@"
 export PYTHONPATH=\$PYTHONPATH:\$CODE_DIR/scripts
-python ci/scripts/$@
+python $CODE_DIR/scripts/$@
