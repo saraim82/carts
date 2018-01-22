a04eca199b0292293ee624b99d184ac708fa4387
Sarai Morales <Sarai_Morales@ultimatesoftware.com>
2018-01-22 14:10:27 -0500

    Modifying run_test.sh

--- ci/scripts/run_test.sh
+++ ci/scripts/run_test.sh
@@ -8,7 +8,7 @@ SSH_OPTS=-oStrictHostKeyChecking=no
 
 
 echo "Testing $1"
-CODE_DIR=$(pwd)
+CODE_DIR=$(cd $SCRIPT_DIR/../..; pwd)
 cp $PWD/test/$1 $PWD/ci/scripts/
 cp -R $PWD/test/util $PWD/ci/scripts/
 echo "$@"
